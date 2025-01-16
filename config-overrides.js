8e661f756f5c0c3c532f4f406b4c20df6ca7b6f5
Richard Freedman <rfreedma@haverford.edu>
2025-01-16 08:44:33 -0500

    update

--- config-overrides.js
+++ config-overrides.js
@@ -3,14 +3,5 @@ const { override, addWebpackExternals } = require('customize-cra');
 module.exports = override(
   addWebpackExternals({
     verovio: 'commonjs verovio',
-  }),
-  // Add polyfills for fs and path
-  (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      'fs': false,
-      'path': false,
-    };
-    return config;
-  }
+  })
 );
--- config-overrides_old.js
+++
@@ -1,7 +0,0 @@
-const { override, addWebpackExternals } = require('customize-cra');
-
-module.exports = override(
-  addWebpackExternals({
-    verovio: 'commonjs verovio',
-  })
-);
