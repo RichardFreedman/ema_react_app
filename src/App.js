5b9db72cbd0474da83dd39f7f67605f7ec0d14fb
Richard Freedman <rfreedma@haverford.edu>
2025-08-13 10:44:02 -0400

    try small

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
     verovio.module.onRuntimeInitialized = async () => {
       const tk = new verovio.toolkit();
       tk.setOptions({
-        'scale': 30,
+        'scale': 10,
         'adjustPageWidth': True,
         'adjustPageHeight': True, 
       });
