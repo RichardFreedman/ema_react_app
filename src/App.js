779eaf4975b503aac9698d2cece0df788eb247e4
Richard Freedman <rfreedma@haverford.edu>
2025-08-13 21:17:48 -0400

    tk

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
       //   adjustPageHeight: true, 
       // });
         tk.setOptions({
-          scale: 8,              // Keep your small music size
+          scale: 15,              // Keep your small music size
           pageWidth: 3000,        // Force a much wider page
           adjustPageWidth: false, // Turn OFF auto-width so it uses our pageWidth
           adjustPageHeight: true, // Keep auto-height
